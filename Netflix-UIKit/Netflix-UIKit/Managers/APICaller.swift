441c7fa3f13a493bf7421b5a60364bdbb126a239
Ozgundogus <43118369+Ozgundogus@users.noreply.github.com>
2023-05-09 00:36:40 +0300

    settings tableview section and created APIManager

---
+++ Netflix-UIKit/Netflix-UIKit/Managers/APICaller.swift
@@ -0,0 +1,18 @@
+//
+//  APICaller.swift
+//  Netflix-UIKit
+//
+//  Created by Ozgun Dogus on 7.05.2023.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "344618ec1f6413483d7c54028ffc3af6"
+}
+
+
+class APICaller {
+    
+        static let shared = APICaller()
+}
