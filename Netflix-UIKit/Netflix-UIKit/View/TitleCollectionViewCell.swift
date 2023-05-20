31a36ba913fe78c63c7462bfadf2c0762e595b0b
Ozgundogus <ozgundogus@outlook.com>
2023-05-20 22:49:44 +0300

    implemented SdWebImage package in project

--- Netflix-UIKit/Netflix-UIKit/View/TitleCollectionViewCell.swift
+++ Netflix-UIKit/Netflix-UIKit/View/TitleCollectionViewCell.swift
@@ -6,16 +6,32 @@
 //
 
 import UIKit
+import SDWebImage
 
 class TitleCollectionViewCell: UICollectionViewCell {
     
     static let identifier = "TitleCollectionViewCell"
     
+    
+    // immutable yapı olduğu için aşağıdaki gibi kullanılır
+    private let posterImageView : UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+        
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        contentView.addSubview(posterImageView)
     }
     
     required init?(coder: NSCoder) {
       fatalError()
+    }
+    //UIView alt sınıfında yer alan alt view'ların konumlarını, boyutlarını veya diğer özelliklerini ayarlamak veya yeniden hesaplamak //için kullanılır
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        posterImageView.frame = bounds
     }
 }
